3b3db0945b576947b7aa4989b7421d8365358dcd
Spencer Wood <spencer@MacBook-010024026104.app.byu.edu>
2015-05-06 16:54:35 -0600

    add new install script

---
+++ install.sh
@@ -0,0 +1,39 @@
+dir=~/dotfiles
+backup_dir=~/dotfiles_backup
+
+files="
+gitignore_global 
+livestreamerrc 
+tmux.conf 
+tmux.osx.conf 
+tmux.reset.conf
+vim 
+vimrc
+vrapper
+zsh 
+zshrc"
+
+echo
+echo "Creating $backup_dir for backup of existing dotfiles"
+mkdir -p $backup_dir
+rm -rf $backup_dir/*
+echo
+
+for file in $files; do
+  if [ -a ~/.$file ]; then
+    echo "Backing up .$file"
+    cp ~/.$file $backup_dir 2> /dev/null
+    rm ~/.$file 2> /dev/null
+  fi
+done
+
+for file in $files; do
+  echo "Symlinking .$file"
+  ln -sf $dir/$file ~/.$file 2> /dev/null
+done
+
+echo
+echo "Updating ~/dotfile submodules"
+git submodule update --init --recursive
+echo "Finished"
+echo
