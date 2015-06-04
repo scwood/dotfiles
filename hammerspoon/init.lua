ec2a4294f153d934a98d90ec6fde5f95ec393404
Spencer Wood <spencercwood@gmail.com>
2015-06-04 11:51:04 -0600

    add application shortcuts

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -14,6 +14,27 @@ hs.alert.show("Config loaded")
 hs.hotkey.bind(hyper, "a", function()
   hs.hints.windowHints()
 end)
+hs.hints.showTitleThresh = 0
+
+--------------------------------------------------------------------------------
+-- application shortcuts
+--------------------------------------------------------------------------------
+
+hs.hotkey.bind(hyper, "t", function()
+  hs.application.launchOrFocus("iTerm")
+end)
+
+hs.hotkey.bind(hyper, "c", function()
+  hs.application.launchOrFocus("Google Chrome")
+end)
+
+hs.hotkey.bind(hyper, "s", function()
+  hs.application.launchOrFocus("Sublime Text")
+end)
+
+hs.hotkey.bind(hyper, "e", function()
+  hs.application.launchOrFocus("Finder")
+end)
 
 --------------------------------------------------------------------------------
 -- window management
@@ -55,5 +76,18 @@ hs.hotkey.bind(hyper, "g", function()
   frame.y = max.y
   frame.w = max.w / 2
   frame.h = max.h
+  win:setFrame(frame)
+end)
+
+-- hyper n -> move to next monitor
+hs.hotkey.bind(hyper, "n", function()
+  local win = hs.window.focusedWindow()
+  local nextScreen = win:screen():next()
+  local max = nextScreen:frame()
+  frame.x = max.e
+  frame.y = max.y
+  frame.w = max.w
+  frame.h = max.h
+  win:moveToScreen(nextScreen)
   win:setFrame(frame)
 end)
