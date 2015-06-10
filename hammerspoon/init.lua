ab68a73dd9728ffca3f82a67adcd8b4e0b97a673
Spencer Wood <spencercwood@gmail.com>
2015-06-10 11:49:51 -0600

    add more window movement

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,96 +1,90 @@
 --------------------------------------------------------------------------------
 -- general
----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
-local hyper = {"shift", "cmd", "alt", "ctrl"}
+-- define hyper key
+local hyper = {'shift', 'cmd', 'alt', 'ctrl'}
+
+-- disable window snapping animation
 hs.window.animationDuration = 0
 
+-- reload conifg file on save
 function reload_config(files)
   hs.reload()
 end
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
-hs.alert.show("Config loaded")
+hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', reload_config):start()
+hs.alert.show('Config loaded')
 
-hs.hotkey.bind(hyper, "a", function()
+-- window hints settings
+hs.hotkey.bind(hyper, 'a', function()
   hs.hints.windowHints()
 end)
 hs.hints.showTitleThresh = 0
 
 --------------------------------------------------------------------------------
--- application shortcuts
---------------------------------------------------------------------------------
-
-hs.hotkey.bind(hyper, "t", function()
-  hs.application.launchOrFocus("iTerm")
-end)
-
-hs.hotkey.bind(hyper, "c", function()
-  hs.application.launchOrFocus("Google Chrome")
-end)
-
-hs.hotkey.bind(hyper, "s", function()
-  hs.application.launchOrFocus("Sublime Text")
-end)
-
-hs.hotkey.bind(hyper, "e", function()
-  hs.application.launchOrFocus("Finder")
-end)
-
-hs.hotkey.bind(hyper, "m", function()
-  hs.application.launchOrFocus("Spotify")
-end)
---------------------------------------------------------------------------------
 -- window management
 --------------------------------------------------------------------------------
 
--- hyper f -> full screen
-hs.hotkey.bind(hyper, "f", function()
-  local win = hs.window.focusedWindow()
-  local frame = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  frame.x = max.x
-  frame.y = max.y
-  frame.w = max.w
-  frame.h = max.h
-  win:setFrame(frame)
+hs.grid.MARGINX = 0
+hs.grid.MARGINY = 0
+hs.grid.GRIDWIDTH = 2
+hs.grid.GRIDHEIGHT = 2
+
+hs.hotkey.bind(hyper, 'f', hs.grid.maximizeWindow)
+hs.hotkey.bind(hyper, 'n', hs.grid.pushWindowNextScreen)
+
+hs.hotkey.bind(hyper, 'h', function()
+  hs.grid.resizeWindowTaller()
+  hs.grid.resizeWindowThinner()
+  hs.grid.pushWindowLeft()
 end)
 
--- hyper d -> left half
-hs.hotkey.bind(hyper, "d", function()
-  local win = hs.window.focusedWindow()
-  local frame = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  frame.x = max.x
-  frame.y = max.y
-  frame.w = max.w / 2
-  frame.h = max.h
-  win:setFrame(frame)
+hs.hotkey.bind(hyper, 'j', function()
+  hs.grid.resizeWindowShorter()
+  hs.grid.resizeWindowWider()
+  hs.grid.pushWindowDown()
 end)
 
--- hyper g -> right half
-hs.hotkey.bind(hyper, "g", function()
-  local win = hs.window.focusedWindow()
-  local frame = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-  frame.x = max.x + (max.w / 2)
-  frame.y = max.y
-  frame.w = max.w / 2
-  frame.h = max.h
-  win:setFrame(frame)
+hs.hotkey.bind(hyper, 'k', function()
+  hs.grid.resizeWindowShorter()
+  hs.grid.resizeWindowWider()
+  hs.grid.pushWindowUp()
 end)
 
--- hyper n -> move to next monitor
-hs.hotkey.bind(hyper, "n", function()
-  local win = hs.window.focusedWindow()
-  local nextScreen = win:screen():next()
-  local max = nextScreen:frame()
-  frame.x = max.e
-  frame.y = max.y
-  frame.w = max.w
-  frame.h = max.h
-  win:moveToScreen(nextScreen)
-  win:setFrame(frame)
+hs.hotkey.bind(hyper, 'l', function()
+  hs.grid.resizeWindowTaller()
+  hs.grid.resizeWindowThinner()
+  hs.grid.pushWindowRight()
+end)
+
+--------------------------------------------------------------------------------
+-- application shortcuts
+--------------------------------------------------------------------------------
+
+hs.hotkey.bind(hyper, 't', function()
+  hs.application.launchOrFocus('iTerm')
+end)
+
+hs.hotkey.bind(hyper, 'c', function()
+  hs.application.launchOrFocus('Google Chrome')
+end)
+
+hs.hotkey.bind(hyper, 's', function()
+  hs.application.launchOrFocus('Sublime Text')
+end)
+
+hs.hotkey.bind(hyper, 'e', function()
+  hs.application.launchOrFocus('Finder')
+end)
+
+hs.hotkey.bind(hyper, 'm', function()
+  hs.application.launchOrFocus('Spotify')
+end)
+
+hs.hotkey.bind(hyper, 'd', function()
+  hs.application.launchOrFocus('Dash')
+end)
+
+hs.hotkey.bind(hyper, 'p', function()
+  hs.application.launchOrFocus('Plug')
 end)
