70fb5eccd108fec318685529b1842f7380579e6a
Spencer Wood <spencercwood@gmail.com>
2015-08-17 20:50:13 -0600

    add corner snapping functionality

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -12,20 +12,36 @@ function snapAllWindows()
   hs.fnutils.map(hs.window.visibleWindows(), hs.grid.snap)
 end
 
-function leftHalf() 
+function snapToLeftHalf() 
   hs.window.focusedWindow():moveToUnit(hs.layout.left50) 
 end
 
-function bottomHalf()
+function snapToBottomHalf()
   hs.window.focusedWindow():moveToUnit({x=0, y=0.5, w=1, h=0.5}) 
 end
 
-function topHalf() 
+function snapToTopHalf() 
   hs.window.focusedWindow():moveToUnit({x=0, y=0, w=1, h=0.5}) 
 end
 
-function rightHalf() 
+function snapToRightHalf() 
   hs.window.focusedWindow():moveToUnit(hs.layout.right50) 
+end
+
+function snapToNWCorner()
+  hs.window.focusedWindow():moveToUnit({x=0, y=0, w=0.5, h=0.5}) 
+end
+
+function snapToNECorner()
+  hs.window.focusedWindow():moveToUnit({x=0.5, y=0, w=0.5, h=0.5}) 
+end
+
+function snapToSWCorner()
+  hs.window.focusedWindow():moveToUnit({x=0, y=0.5, w=0.5, h=0.5}) 
+end
+
+function snapToSECorner()
+  hs.window.focusedWindow():moveToUnit({x=0.5, y=0.5, w=0.5, h=0.5}) 
 end
 
 function moveToNextScreen()
@@ -36,11 +52,15 @@ function moveToPreviousScreen()
   hs.window.focusedWindow():moveOneScreenWest()
 end
 
-hs.hotkey.bind(mash, 'h', leftHalf)
-hs.hotkey.bind(mash, 'j', bottomHalf)
-hs.hotkey.bind(mash, 'k', topHalf)
-hs.hotkey.bind(mash, 'l', rightHalf)
+hs.hotkey.bind(mash, "'", snapAllWindows)
+hs.hotkey.bind(mash, 'h', snapToLeftHalf)
+hs.hotkey.bind(mash, 'j', snapToBottomHalf)
+hs.hotkey.bind(mash, 'k', snapToTopHalf)
+hs.hotkey.bind(mash, 'l', snapToRightHalf)
+hs.hotkey.bind(mash, 'y', snapToNWCorner)
+hs.hotkey.bind(mash, 'u', snapToNECorner)
+hs.hotkey.bind(mash, 'i', snapToSWCorner)
+hs.hotkey.bind(mash, 'o', snapToSECorner)
 hs.hotkey.bind(mash, 'n', moveToNextScreen)
 hs.hotkey.bind(mash, 'p', moveToPreviousScreen)
 hs.hotkey.bind(mash, 'm', hs.grid.maximizeWindow)
-hs.hotkey.bind(mash, "'", snapAllWindows)
